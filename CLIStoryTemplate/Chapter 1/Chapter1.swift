0e8c938db05dd6ef58fa07bc79ededd8a4782d37
Dom Holmes <Dom.carlos29@icloud.com>
2023-10-27 15:34:56 -0400

    the first 3 stops of the qline tour broken down into seperate functions.

--- CLIStoryTemplate/Chapter 1/Chapter1.swift
+++ CLIStoryTemplate/Chapter 1/Chapter1.swift
@@ -5,7 +5,43 @@
 
 import Foundation
 
+    let storyTitle: String = " The Q-line Tour"
+    var numberOfStops = 12
+    var goToNextStop = true
+    var getOfftrain = true
+    let firstStop = "GrandBlvd"
+    let secondStop = "Baltimore"
+    let thirdStop = " Amsterdam"
+    var isTheTrainMoving = true
+    
+
 func chapterOne() {
-    print("So there i was driving down the lodge freeway")
-    //
+    
+    func firstStopQline(){
+        print("Our story begins with First Q-line Stop when riding down Woodward Ave towards hart plaza")
+        print("We  get on the train and read the sign seeing \(firstStop)")
+        print("I see this huge building called the fisher with mayan architecture")
+   // }
+    //if
+        
+    }
+    func secondStopQline() {
+        print ("The train began to move and he soon approached the \(secondStop) stop")
+        print(" I got of the train and went in northern lights for a quick drink")
+        print(" 20 minutes later i head back to the Q-line.")
+        if getOfftrain == false {
+            print("I get off the train and go to northern lights.")
+        }
+    }
+    func thirdStopQLine() {
+        print ("Seeing that this was not as fast as he thought he put his airpods in before reaching the\(thirdStop) stop")
+        print (" at the \(thirdStop) stop I recognized a friend who is heading to the apple academy with me.")
+        print(" I call his name and give him a hand shake")
+    }
+    func threeFirstStops() {
+        firstStopQline()
+        secondStopQline()
+        thirdStopQLine()
+    }
+    threeFirstStops()
 }
